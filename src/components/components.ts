095445a6e7f0875741e4cbf9982df4102c77ea7b
Hannah Hall <hannahkhall13@gmail.com>
2018-01-15 09:21:15 -0600

    component scaffold

---
+++ src/components/components.ts
@@ -0,0 +1,9 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GameComponent } from './game/game.component';
+import { LoginComponent } from './login/login.component';
+
+export {
+  DashboardComponent,
+  GameComponent,
+  LoginComponent
+};
---
+++ src/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from '../../providers/providers';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(public afAuth: AngularFireAuth, public auth: AuthService, private router: Router) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.router.navigate([`dashboard/${user.uid}`]).then(
+          res => console.log(res),
+          err => console.log(err)
+        );
+      }
+    });
+  }
+
+
+
+  title = 'Checkers';
+  user = { email: '', password: '' };
+
+
+  login() {
+    this.auth.login(this.user).then(
+      (res) => {
+        console.log(res);
+      },
+      err => console.log(err)
+    );
+  }
+
+  register() {
+    this.auth.register(this.user).then(
+      (res) => {
+        console.log(res);
+      },
+      err => console.log(err)
+    );
+  }
+
+  ngOnInit() {
+  }
+
+
+}
